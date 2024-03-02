7ef2e532232a7b9a055ab07d95c29eac8271ea63
Bukasj <chukwuebukaedej@gmail.com>
2024-03-02 12:49:53 -0800

    function that sorts an array of integers in ascending order using the Bubble sort algorithm

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -15,32 +15,24 @@ void bubble_sort(int *array, size_t size)
 	size_t i;
 	size_t j;
 	int temp;
-	int swapped;
 
-	if (size < 2)
+	if (array == NULL || size < 2)
 	{
 		return;
 	}
 
 	for (i = 0; i < size - 1; i++)
 	{
-		swapped = 0;
 
 		for (j = 0; j < size; j++)
 		{
-			if (array[j] > array[j + 1] && array[j + 1])
+			if (array[j] > array[j + 1])
 			{
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
 				print_array(array, size);
-
-				/*swapped = 1;*/
 			}
 		}
-		/*if (!swapped)
-		{
-			break;
-		}*/
 	}
 }
--- sort.h
+++ sort.h
@@ -1,6 +1,10 @@
 #ifndef SORT_H
 #define SORT_H
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -10,11 +14,12 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
+void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
@@ -28,5 +33,6 @@ void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+
 
 #endif /* SORT_H */
