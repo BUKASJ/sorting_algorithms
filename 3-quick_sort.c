09439e97eece4486b375a68161c301939222b929
Bukasj <chukwuebukaedej@gmail.com>
2024-03-03 03:04:21 -0800

    function that sorts an array of integers in ascending order using the Quick sort algorithm

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -77,6 +77,7 @@ void quick_sort_recursive(int *array, int low, int high, size_t size)
 
 void quick_sort(int *array, size_t size)
 {
-	if (array && size > 1)
-		quick_sort_recursive(array, 0, size - 1, size);
+	if (!array || size < 2)
+		return;
+	quick_sort_recursive(array, 0, size - 1, size);
 }
