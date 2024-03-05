344b4886eda6f8817bb73ec88c3b14aacc3c3864
Bukasj <chukwuebukaedej@gmail.com>
2024-03-05 08:22:48 -0800

    sorts a doubly linked list of integers in ascending order using the Cocktail shaker sort algorithm

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -2,43 +2,76 @@
 
 /**
  * cocktail_sort_list - Sorts a doubly linked list of integers
- *                      in ascending order using the Cocktail shaker sort
- *                      algorithm
+ *                      in ascending order using Cocktail shaker sort algorithm
  * @list: Pointer to a pointer to the head of the linked list
  */
 
 void cocktail_sort_list(listint_t **list)
 {
-	listint_t *current;
-	int swapped = 1;
+	listint_t *start = *list, *end = NULL;
+	int swapped;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	while (swapped)
-	{
+	do {
 		swapped = 0;
-		for (current = *list; current->next != NULL;
-				current = current->next)
+
+		while (start->next != end)
 		{
-			if (current->n > current->next->n)
+			if (start->n > start->next->n)
 			{
-				swap_nodes(list, current, current->next);
-				swapped = 1;
+				swap_nodes(list, start, start->next);
 				print_list(*list);
+				swapped = 1;
+			}
+			else
+			{
+				start = start->next;
 			}
 		}
-		if (!swapped)
+		if (swapped == 0)
 			break;
-		swapped = 0;
-		for (; current->prev != NULL; current = current->prev)
+		end = start;
+
+		while (start->prev != NULL)
 		{
-			if (current->n < current->prev->n)
+			if (start->n < start->prev->n)
 			{
-				swap_nodes(list, current->prev, current);
-				swapped = 1;
+				swap_nodes(list, start->prev, start);
 				print_list(*list);
+				swapped = 1;
+			}
+			else
+			{
+				start = start->prev;
 			}
 		}
-	}
+	} while (swapped);
+}
+
+/**
+ * swap_nodes - Swaps two nodes in a doubly linked list
+ * @list: Pointer to a pointer to the head of the linked list
+ * @node1: First node to swap
+ * @node2: Second node to swap
+ */
+
+void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2)
+{
+	if (node1 == NULL || node2 == NULL)
+		return;
+
+	if (node1->prev != NULL)
+		node1->prev->next = node2;
+	else
+		*list = node2;
+
+	if (node2->next != NULL)
+		node2->next->prev = node1;
+
+	node1->next = node2->next;
+	node2->prev = node1->prev;
+	node1->prev = node2;
+	node2->next = node1;
 }
